d48d51ff1b668bfcbaa0bdce37622516bf821db5
Scala Steward <me@scala-steward.org>
2019-08-09 14:12:27 +0200

    Update ficus to 1.4.7

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -168,7 +168,7 @@ object Dependencies {
 //    val uuid = "com.eaio.uuid" % "uuid" % "3.4"
     val uuid = "com.github.stephenc.eaio-uuid" % "uuid" % "3.4.0"
     val config = "com.typesafe" % "config" % "1.3.1"
-    val ficus = "com.iheart" %% "ficus" % "1.4.0"
+    val ficus = "com.iheart" %% "ficus" % "1.4.7"
     val shapeless = "com.chuusai" %% "shapeless" % "2.3.2" withSources() withJavadoc()
     val parboiled = "org.parboiled" %% "parboiled" % "2.1.4"
     val inflector = "org.atteo" % "evo-inflector" % "1.2.2"
